fc47030f3ba2e5b1c3813d50eb44ab20c240529b
Angelina <ersovangelina@gmail.com>
2025-05-04 18:46:21 +0200

    AppManager, logger, MyListener

--- src/test/java/com/demoqa/core/TestBase.java
+++ src/test/java/com/demoqa/core/TestBase.java
@@ -1,6 +1,10 @@
 package com.demoqa.core;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,20 +12,23 @@ import java.time.Duration;
 
 public class TestBase {
     public WebDriver driver;
+    protected ApplicationManager app = new ApplicationManager(System.getProperty("browser","chrome"));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 
     @BeforeEach
-    public void init() {
-        driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+    public void init( TestInfo testInfo){
+        driver = app.startTest();
+        logger.info("Start test " + testInfo.getDisplayName());
     }
 
-//    @AfterEach
-//    public void tearDown(){
-//        driver.quit();
-    //   }
+    @AfterEach
+    public void tearDown() {
+        app.stopTest();
+        logger.info("Stop test");
+        logger.info("========================================");
+    }
+
 }
 
